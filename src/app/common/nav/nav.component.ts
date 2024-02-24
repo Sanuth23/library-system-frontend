e8dd71f3bdaae8e0d182bbc626d327d411d64eaf
Sanuth23 <sanuth0000@gmail.com>
2024-02-25 00:04:06 +0530

    add routers for pages

--- src/app/common/nav/nav.component.ts
+++ src/app/common/nav/nav.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../pages/login/login.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [LoginComponent,RouterLink],
+  imports: [LoginComponent,RouterLink,RouterOutlet],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
